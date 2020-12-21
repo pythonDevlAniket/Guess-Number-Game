794ce787955e25471572462330ab8484c94e173d
Aniket Awadan <76273079+pythonDevlAniket@users.noreply.github.com>
2020-12-21 09:39:49 +0530

    Update simple dice game.py

--- simple dice game.py
+++ simple dice game.py
@@ -1,6 +1,6 @@
-import random
+import random  #importing random module
 def dice():
-    luck=random.randint(1,6)
+    luck=random.randint(1,6)  #generate a random integer between 1 to 6
     if luck==6:
         print("\n\t$$$ You are lucky...! You got 6 number in dice.. ")
     else:
@@ -8,6 +8,7 @@ def dice():
 print("\n\t********Welcome to dice game.....Check your luck*******")
 dice()
 while True:
+# while loop stop execution if user hits '0'
     hit=int(input("\n\tDid you like this...? Press 1 to play again and 0 to exit the game...>> "))
     if hit==1:
         dice()
