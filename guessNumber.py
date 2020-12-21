29f1fed99c2a1cc1ddda557f0f7e5bb4d34e66ae
Aniket Awadan <76273079+pythonDevlAniket@users.noreply.github.com>
2020-12-21 09:37:10 +0530

    Update guessNumber.py

--- guessNumber.py
+++ guessNumber.py
@@ -2,19 +2,21 @@
 #If user fails to get that number then he will lose the game.
 import random
 print("\n\t\t........Welcome to my Guess number Program........")
-n=random.randint(1,11)
+n=random.randint(1,11) #this will generate a random integer in range 1 to 11
 chances=1
 print("\n\tGuess a number between 1 to 11: ",end="")
 print("\n\t\tYour Hint is: ",end="")
+
+#The following code is for hint to predict that number
 if n%2==0:
     print('''"It is an Even number"''',"\n")
 else:
     print('''"It is an Odd number"''',"\n")
 print("Lets try it...:",end="")
 
-
+# If you fails to predict code more than or equal to 5 times then you Lose.. 
 while chances<=5:
-    guess=int(input())
+    guess=int(input()) # takes integer as input
     if guess==n:
         print("\n\t.......Finally you got number",n," in",chances,"chances...!")
         chances=chances+1
